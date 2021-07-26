3ab4a7659bccdc4f95b36383d618c3026c75d06b
EnriqueRojasR <enriquer803@gmail.com>
2021-07-25 21:55:05 -0500

    Update ProyectoFinal.cpp

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -762,7 +762,7 @@ int main()
 		//view = camera.GetViewMatrix();
 		//model = glm::mat4(1);
 		//model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-		// model = glm::rotate(model, glm::radians(180.0f) , glm::vec3(0.0f, 1.0f, 0.0f));
+		// // model = glm::rotate(model, glm::radians(180.0f) , glm::vec3(0.0f, 1.0f, 0.0f));
 		//model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
 		//glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 		//Jarron.Draw(lightingShader);
